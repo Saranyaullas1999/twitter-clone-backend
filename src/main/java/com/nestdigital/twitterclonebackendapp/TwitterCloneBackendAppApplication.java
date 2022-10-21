e7b8845eb0825ee9cfeb840e6f3b597d1aae39c6
Saranyaullas1999 <saranyaullas1999@gmail.com>
2022-10-21 12:13:48 +0530

    twitter backend app

---
+++ src/main/java/com/nestdigital/twitterclonebackendapp/TwitterCloneBackendAppApplication.java
@@ -0,0 +1,13 @@
+package com.nestdigital.twitterclonebackendapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwitterCloneBackendAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TwitterCloneBackendAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nestdigital/twitterclonebackendapp/TwitterCloneBackendAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nestdigital.twitterclonebackendapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TwitterCloneBackendAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
